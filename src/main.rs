460b0294f4806e467b0b04a07decf860d9c07d1e
Adam Cimarosti <cimarosti@gmail.com>
2023-05-31 19:14:03 +0100

    Argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use tokio;
+use clap::Parser;
+
+/// Query the TSBS dataset into memory from concurrent connections.
+#[derive(Parser)]
+struct Args {
+    /// QuestDB Host
+    #[clap(long, default_value = "localhost")]
+    host: String,
+
+    /// QuestDB HTTP port
+    #[clap(long, default_value = "9000")]
+    port: u16,
+
+    /// Maximum number of io threads [default: CPU count]
+    #[clap(long)]
+    threads: Option<usize>,
+
+    /// Number of concurrent HTTP requests
+    #[clap(long, default_value = "1")]
+    concurrency: usize,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let _args = Args::parse();
+
     println!("Hello, world!");
     Ok(())
 }
